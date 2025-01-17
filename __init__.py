8d0c1b28cc67a4c5389ad76d83186c7265fec7ad
orencloud <143336512+orencloud@users.noreply.github.com>
2025-01-18 00:44:54 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@
 bl_info = {
     "name": "Secret Paint",
     "author": "orencloud",
-    "version": (1, 7, 0),
+    "version": (1, 7, 1),
     "blender": (4, 2, 0),
     "location": "Object + Target + Q",
     "description": "Paint the selected object on top of the active one",
